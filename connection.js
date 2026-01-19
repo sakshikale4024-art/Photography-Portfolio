598dab0cbb8458fde9d9bccb126134568962fefe
KALE SAKSHI SANJAY <sakshikale4024@gmail.com>
2026-01-19 12:08:38 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 var util = require('util');
 
 var connection = mysql.createConnection({
-    "host":"localhost",
-    "user":"root",
-    "password":"",
-    "database":"photography_portfolio"
+    "host":"bvt7i3eekabncaaicl8x-mysql.services.clever-cloud.com",
+    "user":"u6h6ckztdqgymwyw",
+    "password":"oiz7uLyQTz61h8VndzcB",
+    "database":"bvt7i3eekabncaaicl8x"
 });
 var exe = util.promisify(connection.query).bind(connection);
 
